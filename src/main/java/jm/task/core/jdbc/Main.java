b1b2b47e03ed4e8bbb26fea1d32aef8400281b63
Mikhail Latypov <hodkonem1984@gmail.com>
2024-09-04 14:04:21 +0300

    Реализовал в классе Main алгоритм добавления, обновления, ощищения и удаления таблицы, удаления пользователя по id, получения всех пользователей.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Bill", "Geits", (byte) 55);
+        userService.saveUser("Yuri", "Nikulin", (byte) 40);
+        userService.saveUser("Anton", "Chekhov", (byte) 25);
+        userService.saveUser("Yuri", "Gagarin", (byte) 30);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
