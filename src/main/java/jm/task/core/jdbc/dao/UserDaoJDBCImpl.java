778137a2dcd078e38927385bfdfe42f445187e5c
Mikhail Latypov <hodkonem1984@gmail.com>
2024-09-08 14:18:40 +0300

    Подправил классы UserDaoJDBCImpl и Util, удалил класс PropertiesUtil.java, добавил логгер.

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,140 +2,107 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class UserDaoJDBCImpl implements UserDao {
-    private static final UserDao INSTANCE = new UserDaoJDBCImpl();
 
-    private static final String CREATE_TABLE_USERS_SQL = """
-            CREATE TABLE IF NOT EXISTS users
-            (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(45) NOT NULL,
-                last_name VARCHAR(50) NOT NULL,
-                age SMALLINT NOT NULL
-            )""";
-
-    private static final String DROP_TABLE_USERS_SQL = """
-            DROP TABLE IF EXISTS users;
-                """;
-
-    private static final String SAVE_USER_SQL = """
-            INSERT INTO users(name, last_name, age)
-            VALUES (?,?,?);
-            """;
-
-    private static final String REMOVE_USER_BY_ID_SQL = """
-            DELETE FROM users
-            WHERE id = ?
-            """;
-
-    private static final String GET_ALL_USERS_SQL = """
-            SELECT id,
-                    name,
-                    last_name,
-                    age
-            FROM users
-            """;
-
-    private static final String CLEAN_USERS_TABLE_SQL = """
-            TRUNCATE TABLE users
-            """;
+    private static final Logger logger = LoggerFactory.getLogger(UserDaoJDBCImpl.class);
 
     public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-        try (var connection = Util.get();
-             var prepareStatement = connection.prepareStatement(CREATE_TABLE_USERS_SQL)) {
-            prepareStatement.execute();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        String createTable = "CREATE TABLE IF NOT EXISTS users ("
+                       + "id SERIAL PRIMARY KEY, "
+                       + "name VARCHAR(50), "
+                       + "last_name VARCHAR(50), "
+                       + "age SMALLINT)";
+        executeUpdate(createTable);
     }
 
     public void dropUsersTable() {
-        executeUpdate(DROP_TABLE_USERS_SQL);
+        String dropTableUsers = "DROP TABLE IF EXISTS users";
+        executeUpdate(dropTableUsers);
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (var connection = Util.get()) {
+        String save = "INSERT INTO users (name, last_name, age) VALUES (?, ?, ?)";
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(save)) {
             connection.setAutoCommit(false);
-            try (var preparedStatement = connection.prepareStatement(SAVE_USER_SQL)) {
-                preparedStatement.setString(1, name);
-                preparedStatement.setString(2, lastName);
-                preparedStatement.setByte(3, age);
-                preparedStatement.executeUpdate();
-                connection.commit();
-                System.out.println("User с именем – " + name + " добавлен в базу данных");
-            } catch (SQLException e) {
-                connection.rollback();
-                System.err.println("Error saving user: " + e.getMessage());
-            }
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+            connection.commit();
+            logger.info("User с именем " + name + " добавлен в базу данных");
         } catch (SQLException e) {
-            System.err.println("Connection error: " + e.getMessage());
+            logger.error("Ошибка при сохранении пользователя: ", e);
         }
     }
 
 
     public void removeUserById(long id) {
-        try (var connection = Util.get()) {
+        String removeUser = "DELETE FROM users\n" +
+                            "WHERE id = ?";
+        try (var connection = Util.getConnection()) {
             connection.setAutoCommit(false);
-            try (var preparedStatement = connection.prepareStatement(REMOVE_USER_BY_ID_SQL)) {
+            try (var preparedStatement = connection.prepareStatement(removeUser)) {
                 preparedStatement.setLong(1, id);
                 preparedStatement.executeUpdate();
                 connection.commit();
             } catch (SQLException e) {
                 connection.rollback();
-                e.printStackTrace();
+                logger.error("Ошибка при выполнении запроса: " + removeUser, e);
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.error("Ошибка при удалении пользователя по id: ", e);
         }
     }
 
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
-        try (var connection = Util.get();
-             var preparedStatement = connection.prepareStatement(GET_ALL_USERS_SQL);
-             var resultSet = preparedStatement.executeQuery()) {
+        String getUsers = "SELECT * FROM users";
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(getUsers)) {
             while (resultSet.next()) {
-                users.add(buildUser(resultSet));
+                Long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                String lastName = resultSet.getString("last_name");
+                Byte age = resultSet.getByte("age");
+                User user = new User(name, lastName, age);
+                user.setId(id);
+                users.add(user);
             }
+            logger.info("Все пользователи извлечены из БД");
         } catch (SQLException e) {
-            System.err.println("Error retrieving users: " + e.getMessage());
+            logger.error("Ошибка при извлечении пользователей: ", e);
         }
         return users;
     }
 
+
+    @Override
     public void cleanUsersTable() {
-        executeUpdate(CLEAN_USERS_TABLE_SQL);
+        String truncateTable = "TRUNCATE TABLE users;";
+        executeUpdate(truncateTable);
     }
 
-    private static User buildUser(ResultSet resultSet) throws SQLException {
-        return new User(
-                resultSet.getLong("id"),
-                resultSet.getString("name"),
-                resultSet.getString("last_name"),
-                resultSet.getByte("age")
-        );
-    }
-
-    private void executeUpdate(String query) {
-        try (var connection = Util.get()) {
-            connection.setAutoCommit(false);
-            try (var statement = connection.createStatement()) {
-                statement.executeUpdate(query);
-                connection.commit();
-            } catch (SQLException e) {
-                connection.rollback();
-                System.err.println("Error executing update: " + e.getMessage());
-            }
+    public void executeUpdate(String query) {
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(query);
+            connection.commit();
+            logger.info("Выполнен запрос " + query);
         } catch (SQLException e) {
-            System.err.println("Connection error: " + e.getMessage());
+            logger.error("Ошибка при выполнении запроса: " + query, e);
         }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,23 +1,61 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public final class Util {
     // реализуйте настройку соеденения с БД
-    private static final String URL_KEY = "db.url";
-    private static final String USERNAME_KEY = "db.username";
-    private static final String PASSWORD_KEY = "db.password";
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
 
-    private Util() {
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            connection.setAutoCommit(false);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
     }
 
-    public static Connection get() throws SQLException {
-        return DriverManager.getConnection(
-                PropertiesUtil.get(URL_KEY),
-                PropertiesUtil.get(USERNAME_KEY),
-                PropertiesUtil.get(PASSWORD_KEY)
-        );
+    public static SessionFactory getSessionFactory() {
+        SessionFactory sessionFactory = null;
+        try {
+
+            Configuration configuration = new Configuration();
+            Properties properties = new Properties();
+            properties.put(Environment.DRIVER, "org.postgresql.Driver");
+            properties.put(Environment.URL, URL);
+            properties.put(Environment.USER, USER);
+            properties.put(Environment.PASS, PASSWORD);
+            properties.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+            properties.put(Environment.SHOW_SQL, "true");
+            properties.put(Environment.FORMAT_SQL,"true");
+
+            configuration.setProperties(properties);
+            configuration.addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties())
+                    .build();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        return sessionFactory;
     }
 }
